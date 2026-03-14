1728feaf8f9f3a1b6e64f34bdbf64fc57e64e358
Jyotikaramalli <jyotikaramalli@gmail.com>
2026-03-14 10:53:42 +0530

    Time: 8 ms (7.28%), Space: 9.5 MB (99.15%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s.size()!=t.size())
+        return false;
+        int i=0;
+        while(i<s.size())
+        {
+        if(s[i]!=t[i])
+        return false;
+        i++;
+        }
+        return true;
+    }
+};
