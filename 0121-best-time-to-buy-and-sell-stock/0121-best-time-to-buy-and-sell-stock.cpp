83efb9615852b849f58f854f728a5260d6f21c70
Jyotikaramalli <jyotikaramalli@gmail.com>
2026-08-11 21:12:03 +0530

    Time: 0 ms (100%), Space: 97.5 MB (25.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int maxpro=0;
-        int bb=prices[0];
-        for(int i=1;i<n;i++)
-        {
-            if(bb<prices[i])
-            {
-                maxpro=max(maxpro,prices[i]-bb);
-            }
-            bb=min(bb,prices[i]);
-        }
-        return maxpro;
+        int bb = prices[0];
+    int max_prof = 0;
+    for(int i = 1; i < prices.size(); i++)
+    {
+        if(prices[i] < bb)
+        bb = prices[i];
+        max_prof = max(max_prof, prices[i]-bb);
+    }
+    return max_prof;
     }
 };
