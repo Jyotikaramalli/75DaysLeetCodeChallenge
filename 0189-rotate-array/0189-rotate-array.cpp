e338c9462618145df2aeb0b9ead36643f12320a5
Jyotikaramalli <jyotikaramalli@gmail.com>
2026-08-28 14:24:33 +0530

    Time: 3 ms (24.18%), Space: 263.3 MB (17.76%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+         int m = 0;
+         k = k % n;
+         std::rotate(nums.begin(), nums.end()-k, nums.end());
+
+        
+    }
+};
